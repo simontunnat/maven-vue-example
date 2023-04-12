08cde76db9ab994934c64ce44c46d081e192480e
Simon Tunnat <simon@tunn.at>
2023-04-12 19:54:43 +0200

    Set Webpack / Vue options to fix some reported warnings

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
@@ -12,7 +13,12 @@ module.exports = {
       template: './src/main/frontend/index.html'
     }),
     new MiniCssExtractPlugin(),
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    // setting feature flags, "to get better tree-shaking in the production bundle."
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: false,
+      __VUE_PROD_DEVTOOLS__: false
+    })
   ],
   module: {
     rules: [
@@ -50,5 +56,10 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+    }
   }
 };
