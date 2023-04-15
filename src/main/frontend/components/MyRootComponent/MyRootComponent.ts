2610ac17a9b66d6d9b2f8cdd4df0c81dc77ae8fe
Simon Tunnat <simon@tunn.at>
2023-04-15 11:05:07 +0200

    Allow to mount multiple instances of the MyRootComponent

--- src/main/frontend/components/MyRootComponent/MyRootComponent.ts
+++ src/main/frontend/components/MyRootComponent/MyRootComponent.ts
@@ -2,9 +2,13 @@ import {createApp} from 'vue'
 import MyCounter from "../MyCounter/MyCounter.vue";
 import MyParagraph from "../MyParagraph/MyParagraph.vue";
 
-// If an app contains no template its innerHtml will be taken as its template
-// See: https://vuejs.org/guide/essentials/application.html#mounting-the-app
-const app = createApp({});
-app.component('MyCounter', MyCounter);
-app.component('MyParagraph', MyParagraph);
-app.mount("div[data-vue-app='MyRootComponent']");
+const containers = document.querySelectorAll("div[data-vue-app='MyRootComponent']");
+
+containers.forEach((container) => {
+  // If an app contains no template its innerHtml will be taken as its template
+  // See: https://vuejs.org/guide/essentials/application.html#mounting-the-app
+  const app = createApp({});
+  app.component('MyCounter', MyCounter);
+  app.component('MyParagraph', MyParagraph);
+  app.mount(container);
+});
