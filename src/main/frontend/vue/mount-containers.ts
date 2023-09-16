d7da803b95d2f3bdb5ffcf67ea48f7c350303dc9
Simon Tunnat <simon@tunn.at>
2023-09-16 17:31:21 +0200

    Added examples for using <template> and <script type="text/x-template"> elements as sources for app templates

--- src/main/frontend/vue/mount-containers.ts
+++ src/main/frontend/vue/mount-containers.ts
@@ -2,16 +2,37 @@ import {createApp} from 'vue'
 import MyCounter from "../components/MyCounter/MyCounter.vue";
 import MyParagraph from "../components/MyParagraph/MyParagraph.vue";
 
-const containers = document.querySelectorAll("div[data-vue-app='MyContainer']");
+const appContainers = document.querySelectorAll("div[data-vue-container]");
 
-containers.forEach((container) => {
+const components = {
+  MyCounter,
+  MyParagraph
+};
+
+function getTemplateFromTemplateOrScriptChildElement(appContainer: Element) {
+  // If the container element has a template child element use that one
+  const templateElement = appContainer.querySelector('template');
+  const scriptElement = appContainer.querySelector('script[type="text/x-template"]');
+
+  if (templateElement) {
+    return templateElement.innerHTML;
+  }
+
+  if (scriptElement) {
+    return scriptElement.innerHTML;
+  }
+
+  return '';
+}
+
+appContainers.forEach((appContainer) => {
+  const template = getTemplateFromTemplateOrScriptChildElement(appContainer);
+
   // If an app contains no template its innerHtml will be taken as its template
   // See: https://vuejs.org/guide/essentials/application.html#mounting-the-app
   const app = createApp({
-    components: {
-      MyCounter,
-      MyParagraph
-    }
+    components: components,
+    template: template
   });
-  app.mount(container);
+  app.mount(appContainer);
 });
--- src/main/frontend/vue/mount-sfc-app.ts
+++ src/main/frontend/vue/mount-sfc-app.ts
@@ -4,4 +4,4 @@ import MyApp from '../components/MyApp/MyApp.vue';
 // This is kind of the default usage of Vue:
 // A "app" component using other components and being mounted into a <div> element
 const app = createApp(MyApp);
-app.mount("div[data-vue-app='MyApp']");
+app.mount("div[data-vue-app]");
