a59d14a2f98635be259de616407c39ef6fab6517
Simon Tunnat <simon@tunn.at>
2023-09-17 12:19:38 +0200

    Fix issue with trying to mount the Vue app to a non-existing container

--- frontend/src/main/frontend/vue/mount-sfc-app.ts
+++ frontend/src/main/frontend/vue/mount-sfc-app.ts
@@ -3,5 +3,9 @@ import MyApp from '../components/MyApp/MyApp.vue';
 
 // This is kind of the default usage of Vue:
 // A "app" component using other components and being mounted into a <div> element
-const app = createApp(MyApp);
-app.mount("div[data-vue-app]");
+const container = document.querySelector('div[data-vue-app]');
+
+if(container) {
+  const app = createApp(MyApp);
+  app.mount(container);
+}
