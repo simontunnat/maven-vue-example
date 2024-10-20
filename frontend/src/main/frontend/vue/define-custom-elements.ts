e08a9f3b547d010e18bb397522249ef1945eaf48
Simon Tunnat <simon@tunn.at>
2024-10-20 12:06:46 +0200

    Added example for custom elements with shadow-root disabled

--- frontend/src/main/frontend/vue/define-custom-elements.ts
+++ frontend/src/main/frontend/vue/define-custom-elements.ts
@@ -5,3 +5,10 @@ import MyParagraph from "../components/MyParagraph/MyParagraph.vue";
 // Vue components can be used as web components
 customElements.define('my-counter-web-component', defineCustomElement(MyCounter));
 customElements.define('my-paragraph-web-component', defineCustomElement(MyParagraph));
+
+customElements.define('my-counter-web-component-no-shadow-root', defineCustomElement(MyCounter,{
+  shadowRoot: false
+}));
+customElements.define('my-paragraph-web-component-no-shadow-root', defineCustomElement(MyParagraph, {
+  shadowRoot: false
+}));
